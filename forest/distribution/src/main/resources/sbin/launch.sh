63573fe5009bcd024afbac07784ef57a84661fd1
fj <master_fengj@163.com>
2017-03-17 11:35:28 +0800

    添加启动脚本

---
+++ forest/distribution/src/main/resources/sbin/launch.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+cd `dirname $0`
+source /etc/profile
+source ~/.bash_profile
+source ../bin/envFn.sh
+
+load_args $*
+
+case "$task" in
+    #--f 逗号分隔的配置文件清单
+    # 每个配置文件支持的形式为： resource://{resourcePath};  file://{filePath}; /{filePath}; {resourcePath}
+    forest_actionlog)
+        ../bin/forest_actionlog.sh --f MsgBatchManager.xml
+    ;;
+    *)
+        echo invalid task $task
+    ;;
+esac
