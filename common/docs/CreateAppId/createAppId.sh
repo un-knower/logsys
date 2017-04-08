4b0b219a55bb049e2b07602f6c93eadb6f072997
fj <master_fengj@163.com>
2017-04-08 21:08:55 +0800

    修改执行脚本

--- common/docs/CreateAppId/createAppId.sh
+++ common/docs/CreateAppId/createAppId.sh
@@ -4,14 +4,8 @@
 #   ./createAppId.sh whaley medusa main
 #
 ####################################################################################
-
-SCALA_HOME=${SCALA_HOME:-/usr/local/scala-2.11.5}
-classpath="${SCALA_HOME}/lib/scala-library.jar"
-for file in ./common*.jar
-do
-    classpath="$classpath:$file"
-done
-for file in ../common*.jar
+classpath="./"
+for file in ./*.jar
 do
     classpath="$classpath:$file"
 done
