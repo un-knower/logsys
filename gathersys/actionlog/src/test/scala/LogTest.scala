1840b740b3df2287733c0eb99fceb2fa025c96b6
fj <master_fengj@163.com>
2017-04-14 15:58:25 +0800

    补充ssl站点相关单元测试

--- gathersys/actionlog/src/test/scala/LogTest.scala
+++ gathersys/actionlog/src/test/scala/LogTest.scala
@@ -11,15 +11,6 @@ import scalaj.http._
 
 class LogTest {
 
-    val signKey="92DOV+sOk160j=430+DM!ZzESf@XkEsn#cKanpB$KFB6%D8z4C^xg7cs6&7wn0A4A*iR9M3j)pLs]ll5E9aFlU(dE0[QKxHZzC.CaO/2Ym3|Tk<YyGZR>WuRUmI?x2s:Cg;YEA-hZubmGnWgXE"
-
-    //(ts,md5)
-    def getSignMd5Info(logBody:String):(String,String)={
-        val ts = System.currentTimeMillis().toString
-        val str = ts + logBody + signKey
-        val md5 = DigestUtil.getMD5Str32(str)
-        (ts,md5)
-    }
 
     @Test
     def test: Unit = {
@@ -27,80 +18,17 @@ class LogTest {
         import com.alibaba.fastjson.{JSONObject, JSON}
         val dir = "/Users/fj/workspace/whaley/projects/WhaleyLogSys/gathersys/actionlog/src/test/datas/rawlogs"
 
-        val parseJSONLines = (filepath: String) => {
-            val stream = new FileInputStream(filepath)
-            val source = scala.io.Source.fromInputStream(stream)
-            val info = source.getLines().flatMap(line => {
-                try {
-                    val jsonObj = JSON.parseObject(line)
-                    if (jsonObj.containsKey("tags")) {
-                        var urlPath = jsonObj.getString("urlPath")
-                        if (!urlPath.startsWith("/")) urlPath = "/" + urlPath
-                        if (urlPath.endsWith("/")) urlPath = urlPath.substring(0, urlPath.length - 1)
-                        (jsonObj.getString("host"), jsonObj.getJSONObject("tags").getString("method").toUpperCase(), urlPath, line) :: Nil
-                    } else {
-                        new Array[(String, String, String, String)](0)
-                    }
-                } catch {
-                    case e: Throwable => {
-                        new Array[(String, String, String, String)](0)
-                    }
-                }
-            })
-            info
-        }
-
-        val parseGetLogLines = (filepath: String) => {
-            val stream = new FileInputStream(filepath)
-            val source = scala.io.Source.fromInputStream(stream)
-            val info = source.getLines().map(_.trim).filter(line => {
-                line.length > 0 && !line.startsWith("#")
-            }).flatMap(line => {
-                val fields = line.split(" ")
-                val host = fields(1)
-                val urlPath = fields(7)
-                (host, "GET", urlPath, line) :: Nil
-            })
-            info
-        }
-
-        val aginomotoData = parseJSONLines(s"${dir}/aginomoto.txt")
-        val moretvData = parseJSONLines(s"${dir}/moretv.com.cn.post.txt")
-        val moretvGetData = parseGetLogLines(s"${dir}/moretv.com.cn.get.txt")
+        val aginomotoData = LogTest.parseJSONLines(s"${dir}/aginomoto.txt")
+        val moretvData =  LogTest.parseJSONLines(s"${dir}/moretv.com.cn.post.txt")
+        val moretvGetData =  LogTest.parseGetLogLines(s"${dir}/moretv.com.cn.get.txt")
 
         //aginomotoData.toArray.distinct.sortBy(_._1).foreach(println)
         //moretvData.toArray.distinct.sortBy(_._1).foreach(println)
 
-        val sendHttp = (host: String, method: String, urlPath: String, data: String) => {
-            val signInfo=getSignMd5Info(data)
-            //val url = s"http://${host}:81${urlPath}"
-            val url = s"http://test-wlslog.aginomoto.com:80${urlPath}"
-            val request: HttpRequest = Http(url).method(method).header("forwardhost",host)
-            try {
-                val res = method match {
-                    case "GET" => {
-                        request.asString
-                    }
-                    case "POST" => {
-                        request.header("log-sign-method","md5")
-                            .header("log-sign-version","1.0")
-                            .header("log-sign-ts",signInfo._1)
-                            .header("log-sign-value",signInfo._2)
-                            .header("content-type", "application/json")
-                            .postData(data.getBytes).asString
-                    }
-                }
-                println(s"url:$url ; res:${res}")
-            } catch {
-                case e: Throwable => {
-                    println(s"url:$url, data:$data")
-                    e.printStackTrace()
-                }
-            }
-
-        }
-
-        aginomotoData.foreach(data=>{ sendHttp(data._1,data._2,data._3,data._4) })
+        aginomotoData.foreach(data => {
+            val host="test-wlslog.aginomoto.com"
+            LogTest.sendHttp(host, data._2, data._3, data._4)
+        })
         //moretvData.foreach(data=>{ sendHttp(data._1,data._2,data._3,data._4) })
         //moretvGetData.foreach(data => { sendHttp(data._1, data._2, data._3, data._4) })
     }
@@ -160,4 +88,147 @@ class LogTest {
         }
 
     }
+
+    @Test
+    def testHttpsNginxData: Unit = {
+        val dir = "/Users/fj/workspace/whaley/projects/WhaleyLogSys/gathersys/actionlog/src/test/datas/rawlogs"
+        val aginomotoData  = LogTest.parseJSONLines(s"${dir}/aginomoto.txt")
+        val moretvData = LogTest.parseJSONLines(s"${dir}/moretv.com.cn.post.txt")
+        val moretvGetData = LogTest.parseGetLogLines(s"${dir}/moretv.com.cn.get.txt")
+
+        val testData=aginomotoData.union(moretvData).union(moretvGetData)
+
+        while (true) {
+            testData.foreach(data => {
+                //val host=data._1
+                val host = "wisp.aginomoto.com"
+                val method = data._2
+                val url = data._3
+                val post = data._4
+                LogTest.sendHttp(host, method, url, post)
+                //LogTest.sendHttps(host, method, "/log/boikgpokn78sb95kjtihcg268dc5mlsr", post)
+            })
+        }
+
+
+    }
+}
+
+object LogTest {
+    val signKey = "92DOV+sOk160j=430+DM!ZzESf@XkEsn#cKanpB$KFB6%D8z4C^xg7cs6&7wn0A4A*iR9M3j)pLs]ll5E9aFlU(dE0[QKxHZzC.CaO/2Ym3|Tk<YyGZR>WuRUmI?x2s:Cg;YEA-hZubmGnWgXE"
+
+    /**
+     *
+     * @param filepath
+     * @return [host,method,url,data]
+     */
+    def parseJSONLines(filepath: String): Seq[(String, String, String, String)] = {
+        val stream = new FileInputStream(filepath)
+        val source = scala.io.Source.fromInputStream(stream)
+        val info = source.getLines().flatMap(line => {
+            try {
+                val jsonObj = JSON.parseObject(line)
+                if (jsonObj.containsKey("tags")) {
+                    var urlPath = jsonObj.getString("urlPath")
+                    if (!urlPath.startsWith("/")) urlPath = "/" + urlPath
+                    if (urlPath.endsWith("/")) urlPath = urlPath.substring(0, urlPath.length - 1)
+                    (jsonObj.getString("host"), jsonObj.getJSONObject("tags").getString("method").toUpperCase(), urlPath, line) :: Nil
+                } else {
+                    new Array[(String, String, String, String)](0)
+                }
+            } catch {
+                case e: Throwable => {
+                    new Array[(String, String, String, String)](0)
+                }
+            }
+        }).toSeq
+        info
+    }
+
+    /**
+     *
+     * @param filepath
+     * @return [host,method,url,data]
+     */
+    def parseGetLogLines(filepath: String): Seq[(String, String, String, String)] = {
+        val stream = new FileInputStream(filepath)
+        val source = scala.io.Source.fromInputStream(stream)
+        val info = source.getLines().map(_.trim).filter(line => {
+            line.length > 0 && !line.startsWith("#")
+        }).flatMap(line => {
+            val fields = line.split(" ")
+            val host = fields(1)
+            val urlPath = fields(7)
+            (host, "GET", urlPath, line) :: Nil
+        }).toSeq
+        info
+    }
+
+    //(ts,md5)
+    def getSignMd5Info(logBody: String): (String, String) = {
+        val ts = System.currentTimeMillis().toString
+        val str = ts + logBody + signKey
+        val md5 = DigestUtil.getMD5Str32(str)
+        (ts, md5)
+    }
+
+    def sendHttp(host: String, method: String, urlPath: String, data: String): Unit = {
+        val signInfo = getSignMd5Info(data)
+        //val url = s"http://${host}:81${urlPath}"
+        val url = s"http://${host}:80${urlPath}"
+        val request: HttpRequest = Http(url).method(method).header("forwardhost", host)
+        try {
+            val res = method match {
+                case "GET" => {
+                    request.asString
+                }
+                case "POST" => {
+                    request.header("log-sign-method", "md5")
+                        .header("log-sign-version", "1.0")
+                        .header("log-sign-ts", signInfo._1)
+                        .header("log-sign-value", signInfo._2)
+                        .header("content-type", "application/json")
+                        .postData(data.getBytes).asString
+                }
+            }
+            println(s"url:$url ; res:${res}")
+        } catch {
+            case e: Throwable => {
+                println(s"url:$url, data:$data")
+                e.printStackTrace()
+            }
+        }
+
+    }
+
+    def sendHttps(host: String, method: String, urlPath: String, data: String): Unit = {
+        val signInfo = getSignMd5Info(data)
+        //val url = s"http://${host}:81${urlPath}"
+        val url = s"https://$host${urlPath}"
+        val request: HttpRequest = Http(url).method(method).header("forwardhost", host)
+        try {
+            val res = method match {
+                case "GET" => {
+                    request.asString
+                }
+                case "POST" => {
+                    request.header("log-sign-method", "md5")
+                        .header("log-sign-version", "1.0")
+                        .header("log-sign-ts", signInfo._1)
+                        .header("log-sign-value", signInfo._2)
+                        .header("content-type", "application/json")
+                        .postData(data.getBytes).asString
+                }
+            }
+            println(s"url:$url ; res:${res}")
+        } catch {
+            case e: Throwable => {
+                println(s"url:$url, data:$data")
+                e.printStackTrace()
+            }
+        }
+
+    }
+
+
 }
