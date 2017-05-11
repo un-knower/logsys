6f327b69277cf5621e39097ce10197d0f2eea674
连凯 <lian.kai@whaley.cn>
2017-05-11 16:20:54 +0800

    更新脚本和配置文件

--- common/src/main/resources/filebeat/start_filebeat.sh
+++ common/src/main/resources/filebeat/start_filebeat.sh
@@ -3,21 +3,22 @@
 if [ $# -ne 1 ]
 then
   echo "usage:
-  ./star_filebeat.sh configFileName(without .yml suffix)
+  ./star_filebeat.sh configFileName
 example:
-  ./star_filebeat.sh filebeat_log-raw-boikgpokn78sb95kjhfrendo";
+  ./star_filebeat.sh filebeat_log-raw-boikgpokn78sb95kjhfrendo.yml";
   exit 1;
 fi
 
 configFileName=$1
+logFileName=${configFileName%.*}
 
 FileBeatHome="$( cd "$( dirname "$0"  )" && cd .. && pwd  )"
 
-pro_num=`ps -ef|grep $configFileName.yml |grep -v grep|wc -l`
+pro_num=`ps -ef|grep $configFileName |grep -v grep|wc -l`
 echo "pro_num is $pro_num"
 if [ ${pro_num} -gt 0 ]; then
   echo "filebeat config [configFileName] is already in use,you need to check and kill it first."
   exit 1
 else
-  nohup $FileBeatHome/filebeat -c $FileBeatHome/conf/$configFileName.yml >> /data/logs/filebeat/$configFileName.log 2>&1 &
+  nohup $FileBeatHome/filebeat -c $FileBeatHome/conf/$configFileName >> /data/logs/filebeat/$logFileName.log 2>&1 &
 fi
