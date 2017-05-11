9e3432fa238a12f1ac6034badbca8438cc17a59f
连凯 <lian.kai@whaley.cn>
2017-05-11 17:08:23 +0800

    更新启动脚本

--- common/src/main/resources/filebeat/start_filebeat.sh
+++ common/src/main/resources/filebeat/start_filebeat.sh
@@ -14,10 +14,10 @@ logFileName=${configFileName%.*}
 
 FileBeatHome="$( cd "$( dirname "$0"  )" && cd .. && pwd  )"
 
-pro_num=`ps -ef|grep $configFileName |grep -v grep|wc -l`
-echo "pro_num is $pro_num"
+pro_num=`ps -ef|grep $configFileName |grep -v grep|grep -v $0 | wc -l`
 if [ ${pro_num} -gt 0 ]; then
-  echo "filebeat config [configFileName] is already in use,you need to check and kill it first."
+  echo "filebeat config [$configFileName] is already in use,you need to check and kill it first."
+  ps -ef|grep $configFileName |grep -v grep|grep -v $0
   exit 1
 else
   nohup $FileBeatHome/filebeat -c $FileBeatHome/conf/$configFileName >> /data/logs/filebeat/$logFileName.log 2>&1 &
