1f8345b958c7805fe5d8e67166a66d5ca857acaf
fj <master_fengj@163.com>
2017-06-27 14:05:21 +0800

    模块代码提交

--- ods-view-metadata/src/main/sbin/launch.sh
+++ ods-view-metadata/src/main/sbin/launch.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+#
+# example:  sh ./launch.sh --taskId=task1 --disableGenerateDDLAndDML=false --disableExecuteDDL=false --disableExecuteDML=false
+#
+
 cd `dirname $0`
 pwd=`pwd`
 
@@ -14,41 +18,6 @@ export gcFile=/data/logs/ods-view-metadata/gc_ods-view-metadata.log
 echo "pid file: ${pidFile}"
 echo "log file: ${logFile}"
 echo "gc file: ${gcFile}"
-echo "groupId: ${groupId}"
 
-case "$cmd" in
-    start)
+../bin/ods_view_metadata.sh $@
 
-        if [ -f ${pidFile} ]; then
-          pid=$(cat ${pidFile})
-          pid=`ps -ef|grep $pid|grep -v grep|awk '{print $2}'`
-          if [ -n "$pid" ]; then
-            echo "task always running! pid=$pid"
-            exit
-          fi
-        fi
-
-        set -x
-        nohup ../bin/ods_view_metadata.sh $@ >> ${logFile} 2>&1 &
-        set +x
-    ;;
-    stop)
-        if [ -f $pidFile ]; then
-            pidV=`cat $pidFile`
-            pid=`ps -ef|grep $pidV|grep -v grep|awk '{print $2}'`
-            if [ -n "$pid" ]; then
-                kill $pid
-                ret=$?
-                echo "kill process $pid,ret=$ret"
-            else
-                echo "process not exists.pid:$pidV"
-            fi
-
-        else
-            echo "pid file $pidFile not exists."
-        fi
-    ;;
-    *)
-        echo invalid cmd $cmd
-    ;;
-esac
