0f761b61e139e506877a2edb13b4410737ca06df
fj <master_fengj@163.com>
2017-06-27 14:58:51 +0800

    模块代码提交

--- ods-view-metadata/src/main/bin/ods_view_metadata.sh
+++ ods-view-metadata/src/main/bin/ods_view_metadata.sh
@@ -5,17 +5,6 @@ pwd=`pwd`
 
 source ./envFn.sh
 
-if [ -z "${pidFile}" ]; then
-    echo "pidFile required!"
-fi
-if [ -z "$logFile" ]; then
-    echo "logFile required!"
-fi
-if [ -z "$gcFile" ]; then
-    echo "gcFile required!"
-fi
-
-
 path='../conf'
 for file in ../lib/*.jar
 do
@@ -26,21 +15,18 @@ if [ -n "$CLASSPATH" ]; then
 else
    classpath="$path"
 fi
-if [ -f "/data/apps/azkaban/share/libs/phoenix-4.10.0-HBase-1.2.jar" ]; then
-    classpath="$classpath:/data/apps/azkaban/share/libs/phoenix-4.10.0-HBase-1.2.jar"
+if [ -f "/data/apps/azkaban/share/libs/phoenix-4.10.0-HBase-1.2-thin-client.jar" ]; then
+    classpath="$classpath:/data/apps/azkaban/share/libs/phoenix-4.10.0-HBase-1.2-thin-client.jar"
 fi
-if [ -f "/data/apps/azkaban/share/libs/hive-jdbc-2.1.1.jar" ]; then
+if [ -f "/data/apps/azkaban/share/libs/hive-jdbc.jar" ]; then
     classpath="$classpath:/data/apps/azkaban/share/libs/hive-jdbc.jar"
 fi
 
 javaXmx=${javaXmx:-4096m}
 javaXms=${javaXms:-1024m}
+set -x
 java -Xmx${javaXmx} -Xms${javaXms} -server -XX:+UseParNewGC -XX:+UseConcMarkSweepGC \
  -XX:+CMSClassUnloadingEnabled -XX:+CMSScavengeBeforeRemark \
  -XX:+DisableExplicitGC -Djava.awt.headless=true \
- -XX:+PrintGCApplicationStoppedTime -XX:+PrintGCDateStamps \
- -Xloggc:$gcFile \
- -Dlog4j.logFile=$logFile \
- -Dlog4j.configuration=file:$pwd/../conf/log4j.properties \
  -classpath $classpath  \
  cn.whaley.bi.logsys.metadata.Application $@
--- ods-view-metadata/src/main/sbin/launch.sh
+++ ods-view-metadata/src/main/sbin/launch.sh
@@ -10,14 +10,5 @@ pwd=`pwd`
 source ../bin/envFn.sh
 load_args $*
 
-export pidFile=${pwd}/../logs/ods-view-metadata.pid
-export logFile=/data/logs/ods-view-metadata/ods-view-metadata.log
-export gcFile=/data/logs/ods-view-metadata/gc_ods-view-metadata.log
-
-
-echo "pid file: ${pidFile}"
-echo "log file: ${logFile}"
-echo "gc file: ${gcFile}"
-
 sh ../bin/ods_view_metadata.sh $@
 
