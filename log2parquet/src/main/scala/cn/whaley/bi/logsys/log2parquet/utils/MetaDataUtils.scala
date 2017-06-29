7f1cacbad4b8d3a681b93d1ed00d8239908b1672
fj <master_fengj@163.com>
2017-06-29 14:18:12 +0800

    模块代码提交

--- log2parquet/src/main/scala/cn/whaley/bi/logsys/log2parquet/utils/MetaDataUtils.scala
+++ log2parquet/src/main/scala/cn/whaley/bi/logsys/log2parquet/utils/MetaDataUtils.scala
@@ -2,160 +2,349 @@ package cn.whaley.bi.logsys.log2parquet.utils
 
 import java.io.File
 
+import com.alibaba.fastjson.{JSONObject, JSON}
+import org.apache.spark.rdd.RDD
+
+import scala.collection.mutable.ArrayBuffer
+import scala.util.matching.Regex
+import scala.collection.JavaConverters._
+
 /**
-  * Created by baozhiwang on 2017/6/27.
-  */
+ * Created by baozhiwang on 2017/6/27.
+ */
 object MetaDataUtils {
 
-  /**
-    * 用来获得appId->输出路径的Map,例如：
-    * boikgpokn78sb95ktmsc1bnkechpgj9l->log_medusa_main3x_${log_type}_${event_id}/key_day=${key_day}/key_hour=${key_hour}
-    */
-  def getAppId2OutputPathTemplateMap: scala.collection.mutable.HashMap[String,String] ={
-    val appId2OutputPathTemplateMap = scala.collection.mutable.HashMap.empty[String,String]
-    val appId2TableNameMap =MetaDataUtils.getAppId2TableNameMap
-    val appId2PartitionName=MetaDataUtils.getAppId2PartitionName
+    /**
+     * 用来获得appId->输出路径的Map,例如：
+     * boikgpokn78sb95ktmsc1bnkechpgj9l->log_medusa_main3x_${log_type}_${event_id}/key_day=${key_day}/key_hour=${key_hour}
+     */
+    def getAppId2OutputPathTemplateMap: scala.collection.mutable.HashMap[String, String] = {
+        val appId2OutputPathTemplateMap = scala.collection.mutable.HashMap.empty[String, String]
+        val appId2TableNameMap = MetaDataUtils.getAppId2TableNameMap
+        val appId2PartitionName = MetaDataUtils.getAppId2PartitionName
 
-    val list=getAllAppId
-    list.foreach(appId=>{
-      val tableName=appId2TableNameMap(appId)
-      val partitionName=appId2PartitionName(appId)
-      val outputPathTemplate=tableName+File.separator+partitionName
-      appId2OutputPathTemplateMap.put(appId,outputPathTemplate)
-    })
-    appId2OutputPathTemplateMap
-  }
-
-  /**
-    * 用来获得appId->表名称的Map,例如：
-    * boikgpokn78sb95ktmsc1bnkechpgj9l->log_medusa_main3x_${log_type}_${event_id}
-    */
-  def getAppId2TableNameMap: scala.collection.mutable.HashMap[String,String] ={
-    val appId2TableNameMap = scala.collection.mutable.HashMap.empty[String,String]
-    val list=getAllAppId
-    list.foreach(appId=>{
-      val tableName=getTableNameByConfig(appId)
-      appId2TableNameMap.put(appId,tableName)
-    })
-    appId2TableNameMap
-  }
-
-  /**
-    * 用来获得appId->分区名称的Map,例如：
-    * boikgpokn78sb95ktmsc1bnkechpgj9l->key_day=${key_day}/key_hour=${key_hour}
-    */
-  def getAppId2PartitionName: scala.collection.mutable.HashMap[String,String] = {
-    val appId2PartitonNameMap = scala.collection.mutable.HashMap.empty[String,String]
-    val list=getAllAppId
-    list.foreach(appId=>{
-      val partitionName=getPartitionNameByConfig(appId)
-      appId2PartitonNameMap.put(appId,partitionName)
-    })
-    appId2PartitonNameMap
-  }
-
-  def getPartitionNameByConfig(appId:String): String ={
-    val fieldName="FIELDNAME"
-    val fieldDefault="FIELDDEFAULT"
-
-    val globalMap = scala.collection.mutable.HashMap.empty[String,String]
-    val globalList = scala.collection.mutable.ListBuffer.empty[String]
-    val globalSet = scala.collection.mutable.Set.empty[String]
-
-    val sqlGlobalConfig=
-      s"""select * from metadata.applog_key_field_desc
-          |where
-          |      ISDELETED=false   and
-          |      FIELDFLAG=2       and
-          |      (APPID='ALL' or APPID='$appId')
-          |order by FIELDORDER""".stripMargin
-    val stat = PhoenixUtils.getStatement
-    val result=stat.executeQuery(sqlGlobalConfig)
-    while(result.next()){
-      val FIELDNAME_VAR=result.getString(fieldName)
-      val FIELDDEFAULT_VAR=result.getString(fieldDefault)
-      if(!globalSet.contains(FIELDNAME_VAR)){
-        globalSet.add(FIELDNAME_VAR)
-        globalList.+=(FIELDNAME_VAR)
-      }
-      globalMap.put(FIELDNAME_VAR,FIELDDEFAULT_VAR)
+        val list = getAllAppId
+        list.foreach(appId => {
+            val tableName = appId2TableNameMap(appId)
+            val partitionName = appId2PartitionName(appId)
+            val outputPathTemplate = tableName + File.separator + partitionName
+            appId2OutputPathTemplateMap.put(appId, outputPathTemplate)
+        })
+        appId2OutputPathTemplateMap
     }
 
-    val appIdTableName=StringBuilder.newBuilder
-    globalList.foreach(e=>{
-      val eValue=globalMap.get(e).get
-      if(null==eValue){
-        appIdTableName.append(e+"=${"+e+"}")
-      }else{
-        appIdTableName.append(eValue+"="+eValue)
-      }
-      appIdTableName.append(File.separator)
-    })
-    var tableName=appIdTableName.toString()
-    if(!tableName.isEmpty&&tableName.endsWith(File.separator)){
-      tableName=tableName.substring(0,tableName.length-1)
-    }
-    tableName
-  }
-
-
-  def getAllAppId():scala.collection.mutable.ListBuffer[String]={
-    val sqlGlobalConfig= s"""select distinct APPID
-                             |from metadata.applog_key_field_desc
-                             |where ISDELETED=false""".stripMargin
-    val stat = PhoenixUtils.getStatement
-    val result=stat.executeQuery(sqlGlobalConfig)
-    val list=scala.collection.mutable.ListBuffer.empty[String]
-    while(result.next()){
-      val appId=result.getString("APPID")
-      list.append(appId)
-    }
-    list
-  }
-
-  def getTableNameByConfig(appId:String): String ={
-    val fieldName="FIELDNAME"
-    val fieldDefault="FIELDDEFAULT"
-    val table_split_string="_"
-
-    val globalMap = scala.collection.mutable.HashMap.empty[String,String]
-    val globalList = scala.collection.mutable.ListBuffer.empty[String]
-    val globalSet = scala.collection.mutable.Set.empty[String]
-
-    val sqlGlobalConfig=
-      s"""select * from metadata.applog_key_field_desc
-          |where
-          |      ISDELETED=false   and
-          |      FIELDFLAG=1       and
-          |      (APPID='ALL' or APPID='$appId')
-          |order by FIELDORDER""".stripMargin
-    val stat = PhoenixUtils.getStatement
-    val result=stat.executeQuery(sqlGlobalConfig)
-    while(result.next()){
-      val FIELDNAME_VAR=result.getString(fieldName)
-      val FIELDDEFAULT_VAR=result.getString(fieldDefault)
-      if(!globalSet.contains(FIELDNAME_VAR)){
-        globalSet.add(FIELDNAME_VAR)
-        globalList.+=(FIELDNAME_VAR)
-      }
-      globalMap.put(FIELDNAME_VAR,FIELDDEFAULT_VAR)
+    /**
+     * 用来获得appId->表名称的Map,例如：
+     * boikgpokn78sb95ktmsc1bnkechpgj9l->log_medusa_main3x_${log_type}_${event_id}
+     */
+    def getAppId2TableNameMap: scala.collection.mutable.HashMap[String, String] = {
+        val appId2TableNameMap = scala.collection.mutable.HashMap.empty[String, String]
+        val list = getAllAppId
+        list.foreach(appId => {
+            val tableName = getTableNameByConfig(appId)
+            appId2TableNameMap.put(appId, tableName)
+        })
+        appId2TableNameMap
     }
 
-    val appIdTableName=StringBuilder.newBuilder
-    globalList.foreach(e=>{
-      val eValue=globalMap.get(e).get
-      if(null==eValue){
-        appIdTableName.append("${"+e+"}")
-      }else{
-        appIdTableName.append(eValue)
-      }
-      appIdTableName.append(table_split_string)
-    })
-    var tableName=appIdTableName.toString()
-    if(!tableName.isEmpty&&tableName.endsWith(table_split_string)){
-      tableName=tableName.substring(0,tableName.length-1)
+    /**
+     * 用来获得appId->分区名称的Map,例如：
+     * boikgpokn78sb95ktmsc1bnkechpgj9l->key_day=${key_day}/key_hour=${key_hour}
+     */
+    def getAppId2PartitionName: scala.collection.mutable.HashMap[String, String] = {
+        val appId2PartitonNameMap = scala.collection.mutable.HashMap.empty[String, String]
+        val list = getAllAppId
+        list.foreach(appId => {
+            val partitionName = getPartitionNameByConfig(appId)
+            appId2PartitonNameMap.put(appId, partitionName)
+        })
+        appId2PartitonNameMap
     }
-    tableName
-  }
+
+    def getPartitionNameByConfig(appId: String): String = {
+        val fieldName = "FIELDNAME"
+        val fieldDefault = "FIELDDEFAULT"
+
+        val globalMap = scala.collection.mutable.HashMap.empty[String, String]
+        val globalList = scala.collection.mutable.ListBuffer.empty[String]
+        val globalSet = scala.collection.mutable.Set.empty[String]
+
+        val sqlGlobalConfig =
+            s"""select * from metadata.applog_key_field_desc
+               |where
+               |ISDELETED=false   and
+               |FIELDFLAG=2       and
+               |(APPID='ALL' or APPID='$appId')
+                                               |order by FIELDORDER""".stripMargin
+        val stat = PhoenixUtils.getStatement
+        val result = stat.executeQuery(sqlGlobalConfig)
+        while (result.next()) {
+            val FIELDNAME_VAR = result.getString(fieldName)
+            val FIELDDEFAULT_VAR = result.getString(fieldDefault)
+            if (!globalSet.contains(FIELDNAME_VAR)) {
+                globalSet.add(FIELDNAME_VAR)
+                globalList.+=(FIELDNAME_VAR)
+            }
+            globalMap.put(FIELDNAME_VAR, FIELDDEFAULT_VAR)
+        }
+
+        val appIdTableName = StringBuilder.newBuilder
+        globalList.foreach(e => {
+            val eValue = globalMap.get(e).get
+            if (null == eValue) {
+                appIdTableName.append(e + "=${" + e + "}")
+            } else {
+                appIdTableName.append(eValue + "=" + eValue)
+            }
+            appIdTableName.append(File.separator)
+        })
+        var tableName = appIdTableName.toString()
+        if (!tableName.isEmpty && tableName.endsWith(File.separator)) {
+            tableName = tableName.substring(0, tableName.length - 1)
+        }
+        tableName
+    }
+
+
+    def getAllAppId(): scala.collection.mutable.ListBuffer[String] = {
+        val sqlGlobalConfig = s"""select distinct APPID
+                                 |from metadata.applog_key_field_desc
+                                 |where ISDELETED=false""".stripMargin
+        val stat = PhoenixUtils.getStatement
+        val result = stat.executeQuery(sqlGlobalConfig)
+        val list = scala.collection.mutable.ListBuffer.empty[String]
+        while (result.next()) {
+            val appId = result.getString("APPID")
+            list.append(appId)
+        }
+        list
+    }
+
+    def getTableNameByConfig(appId: String): String = {
+        val fieldName = "FIELDNAME"
+        val fieldDefault = "FIELDDEFAULT"
+        val table_split_string = "_"
+
+        val globalMap = scala.collection.mutable.HashMap.empty[String, String]
+        val globalList = scala.collection.mutable.ListBuffer.empty[String]
+        val globalSet = scala.collection.mutable.Set.empty[String]
+
+        val sqlGlobalConfig =
+            s"""select * from metadata.applog_key_field_desc
+               |where
+               |ISDELETED=false   and
+               |FIELDFLAG=1       and
+               |(APPID='ALL' or APPID='$appId')
+                                               |order by FIELDORDER""".stripMargin
+        val stat = PhoenixUtils.getStatement
+        val result = stat.executeQuery(sqlGlobalConfig)
+        while (result.next()) {
+            val FIELDNAME_VAR = result.getString(fieldName)
+            val FIELDDEFAULT_VAR = result.getString(fieldDefault)
+            if (!globalSet.contains(FIELDNAME_VAR)) {
+                globalSet.add(FIELDNAME_VAR)
+                globalList.+=(FIELDNAME_VAR)
+            }
+            globalMap.put(FIELDNAME_VAR, FIELDDEFAULT_VAR)
+        }
+
+        val appIdTableName = StringBuilder.newBuilder
+        globalList.foreach(e => {
+            val eValue = globalMap.get(e).get
+            if (null == eValue) {
+                appIdTableName.append("${" + e + "}")
+            } else {
+                appIdTableName.append(eValue)
+            }
+            appIdTableName.append(table_split_string)
+        })
+        var tableName = appIdTableName.toString()
+        if (!tableName.isEmpty && tableName.endsWith(table_split_string)) {
+            tableName = tableName.substring(0, tableName.length - 1)
+        }
+        tableName
+    }
+
+    /**
+     * 查询字段特例表
+     * @return Seq[(logPathReg,fieldNameReg,specialType,specialValue,specialOrder)]
+     */
+    def queryAppLogSpecialFieldDescConf(): Seq[(Regex, Regex, String, String, Int)] = {
+        val sql =
+            s"""
+               |select *
+               |from metadata.applog_special_field_desc
+               |where isDeleted=false
+             """.stripMargin
+        val datas = PhoenixUtils.query(sql)
+        datas.map(row => (row.getString("logPathReg".toUpperCase()).r
+            , row.getString("fieldNameReg".toUpperCase()).r
+            , row.getString("specialType".toUpperCase())
+            , row.getString("specialValue".toUpperCase())
+            , row.getInteger("specialOrder".toUpperCase()).toInt))
+    }
+
+    /**
+     * 解析合并了ALL全局配置的字段配置
+     * @param fieldFlag
+     * @return Map[appId, List[(appId, fieldName, fieldDefault, fieldOrder)]
+     */
+    def resolveAppLogKeyFieldDescConfig(fieldFlag: Int): Map[String, List[(String, String, String, Int)]] = {
+        val sql = s"""select *
+                     |from metadata.applog_key_field_desc
+                     |where ISDELETED=false and FIELDFLAG=${fieldFlag}""".stripMargin
+        val datas = PhoenixUtils.query(sql)
+        resolveAppLogKeyFieldDescConfig(datas)
+    }
+
+    /**
+     * 解析某一类合并了ALL全局配置的字段配置
+     * @param confs 某一类型fieldFlag的配置项
+     * @return Map[appId, List[(appId, fieldName, fieldDefault, fieldOrder)]
+     */
+    def resolveAppLogKeyFieldDescConfig(confs: Seq[JSONObject]): Map[String, List[(String, String, String, Int)]] = {
+        val rs = confs.map(row => (row.getString("APPID"), row.getString("FIELDNAME"), row.getString("FIELDDEFAULT"), row.getInteger("FIELDORDER").toInt))
+        rs.map(row => row._1).distinct.map(id => {
+            val fields = rs.filter(row => row._1 == "ALL" || row._1 == id)
+                .groupBy(row => row._4).map(group => {
+                //同一顺序只允许一个字段名
+                val size = group._2.map(item => item._2).distinct.size
+                assert(size == 1)
+                //合并,最多两行,ALL行和appId行
+                if (size == 1 || group._2.head._1 != "ALL") {
+                    group._2(0)
+                } else {
+                    group._2(1)
+                }
+            }).toList.sortBy(row => row._4)
+            (id, fields)
+        }).toMap
+    }
+
+
+    /**
+     * 对RDD的每条记录解析其输出路径,格式错误的行将被忽略
+     * @param rdd
+     * @return
+     */
+    def parseLogStrRddPath(rdd: RDD[String]): RDD[(String, JSONObject)] = {
+        val jsonObjRdd = rdd.map(row => {
+            try {
+                Some(JSON.parseObject(row))
+            }
+            catch {
+                case _: Throwable => {
+                    None
+                }
+            }
+        }).filter(row => row.isDefined).map(row => row.get)
+        parseLogObjRddPath(jsonObjRdd)
+    }
+
+    /**
+     * 对RDD的每条记录解析其输出路径
+     * @param rdd
+     * @return
+     */
+    def parseLogObjRddPath(rdd: RDD[JSONObject]): RDD[(String, JSONObject)] = {
+        val dbNameFieldMap = resolveAppLogKeyFieldDescConfig(0)
+        val tabNameFieldMap = resolveAppLogKeyFieldDescConfig(1)
+        val parFieldMap = resolveAppLogKeyFieldDescConfig(2)
+        rdd.map(jsonObj => parseLogObjPath(jsonObj, dbNameFieldMap, tabNameFieldMap, parFieldMap))
+    }
+
+    /**
+     * 解析某条日志记录的输出路径
+     * @param logObj
+     * @return
+     */
+    def parseLogObjPath(logObj: JSONObject
+                        , dbNameFieldMap: Map[String, List[(String, String, String, Int)]]
+                        , tabNameFieldMap: Map[String, List[(String, String, String, Int)]]
+                        , parFieldMap: Map[String, List[(String, String, String, Int)]]
+                           ): (String, JSONObject) = {
+        val appId = logObj.getString("appId")
+        val dbNameFields = dbNameFieldMap.get(appId)
+        val tabNameFields = tabNameFieldMap.get(appId)
+        val parFields = parFieldMap.get(appId)
+
+        val dbNameStr = getOrDefault(logObj, dbNameFields)
+        val tabNameStr = getOrDefault(logObj, tabNameFields)
+        val parStr = getOrDefault(logObj, parFields)
+
+        //未设置的部分将被忽略
+        val path = (dbNameStr :: tabNameStr :: parStr :: Nil).filter(item => item != "").mkString("/")
+        (path, logObj)
+
+    }
+
+    //优先级: jsonObj字段值 -> conf字段值 , 如果两者都为空,则忽略该字段
+    def getOrDefault(jsonObj: JSONObject, conf: Option[List[(String, String, String, Int)]]): String = {
+        if (conf.isDefined) {
+            conf.get.map(field => {
+                val fieldName = field._2
+                var fieldDefault = field._3
+                val fieldOrder = field._4
+                if (jsonObj.containsKey(fieldName)
+                    && jsonObj.get(fieldName) != null
+                    && jsonObj.get(fieldName).toString.trim.length > 0) {
+                    fieldDefault = jsonObj.get(fieldName).toString
+                }
+                if (fieldDefault != null && fieldDefault.trim.length > 0) {
+                    Some((fieldName, fieldDefault, fieldOrder))
+                } else {
+                    None
+                }
+            }).filter(item => item.isDefined).map(item => item.get).sortBy(item => item._3).mkString("_")
+        } else {
+            ""
+        }
+    }
+
+
+    /**
+     * 解析字段特例规则库
+     * @param rdd
+     * @return Map[logPath,(字段重命名清单,字段黑名单,行过滤器)]
+     */
+    def parseSpecialRules(rdd: RDD[(String, JSONObject)]): Map[String, (Seq[(String, String)], Seq[String], Seq[(String, String)])] = {
+
+        //特例字段配置数据
+        val specialFieldDescConf = queryAppLogSpecialFieldDescConf
+
+        //路径及其所有字段集合
+        val pathAndFields = rdd.map(row => (row._1, row._2.keySet().asScala)).reduceByKey((set1, set2) => set1 ++ set2).collect()
+
+        val rules = pathAndFields.map(item => {
+            val path = item._1
+            val fields = item._2
+
+            //匹配当前路径的配置,且排序值最小的一组配置
+            var pathSpecialConf = specialFieldDescConf.filter(conf => conf._1.findFirstMatchIn(path).isDefined)
+            val order = pathSpecialConf.minBy(conf => conf._5)
+            pathSpecialConf = pathSpecialConf.filter(conf => conf._5 == order)
+
+            //字段重命名: Seq[(源字段名,字段目标名)]
+            val rename = pathSpecialConf.filter(conf => conf._3 == "rename").flatMap(conf => {
+                fields.filter(field => conf._2.findFirstMatchIn(field).isDefined).map(field => (field, conf._4))
+            })
+
+            //字段黑名单: Seq[源字段名]
+            val blackList = pathSpecialConf.filter(conf => conf._3 == "blackList").flatMap(conf => {
+                fields.filter(field => conf._2.findFirstMatchIn(field).isDefined)
+            })
+
+            //行过滤器: Seq[(字段名,字段值)]
+            val rowBlackFilter = pathSpecialConf.filter(conf => conf._3 == "rowBlackFilter").flatMap(conf => {
+                fields.filter(field => conf._2.findFirstMatchIn(field).isDefined).map(field => (field, conf._4))
+            })
+
+            (path, (rename, blackList, rowBlackFilter))
+        }).toMap
+
+        rules
+
+    }
+
 
 }
--- log2parquet/src/main/scala/cn/whaley/bi/logsys/log2parquet/utils/PhoenixUtils.scala
+++ log2parquet/src/main/scala/cn/whaley/bi/logsys/log2parquet/utils/PhoenixUtils.scala
@@ -2,31 +2,59 @@ package cn.whaley.bi.logsys.log2parquet.utils
 
 import java.sql.{Statement, Connection, DriverManager}
 
+import com.alibaba.fastjson.{JSONObject, JSONArray}
+
+import scala.collection.mutable.ArrayBuffer
+
 /**
-  * Created by michael on 2017/6/13.
-  */
+ * Created by michael on 2017/6/13.
+ */
 object PhoenixUtils {
 
-  /**It is not necessary to pool Phoenix JDBC Connections.
-    * https://phoenix.apache.org/faq.html#Should_I_pool_Phoenix_JDBC_Connections */
-  def getConnection:Connection={
-    Class.forName("org.apache.phoenix.jdbc.PhoenixDriver")
-    val con=DriverManager.getConnection("jdbc:phoenix:bigdata-cmpt-128-1,bigdata-cmpt-128-13,bigdata-cmpt-128-25:2181")
-    con
-  }
+    /** It is not necessary to pool Phoenix JDBC Connections.
+      * https://phoenix.apache.org/faq.html#Should_I_pool_Phoenix_JDBC_Connections */
+    def getConnection: Connection = {
+        Class.forName("org.apache.phoenix.jdbc.PhoenixDriver")
+        val con = DriverManager.getConnection("jdbc:phoenix:bigdata-cmpt-128-1,bigdata-cmpt-128-13,bigdata-cmpt-128-25:2181")
+        con
+    }
 
 
-  def getStatement:Statement={
-    Class.forName("org.apache.phoenix.jdbc.PhoenixDriver")
-    val con=DriverManager.getConnection("jdbc:phoenix:bigdata-cmpt-128-1,bigdata-cmpt-128-13,bigdata-cmpt-128-25:2181")
-    val stat=con.createStatement()
-    stat
-  }
+    def getStatement: Statement = {
+        Class.forName("org.apache.phoenix.jdbc.PhoenixDriver")
+        val con = DriverManager.getConnection("jdbc:phoenix:bigdata-cmpt-128-1,bigdata-cmpt-128-13,bigdata-cmpt-128-25:2181")
+        val stat = con.createStatement()
+        stat
+    }
 
-  def closeConnection(con:Connection)={
-   if(null!=con){
-    con.close()
-   }
-  }
+    def closeConnection(con: Connection) = {
+        if (null != con) {
+            con.close()
+        }
+    }
+
+    /**
+     * 执行查询,并以Seq[JSONObject]的形式返回结果
+     *
+     * @param sql
+     * @return
+     */
+    def query(sql: String): Seq[JSONObject] = {
+        val statement = getStatement
+        val rs = statement.executeQuery(sql)
+        val metadata = rs.getMetaData
+        val result = new ArrayBuffer[JSONObject]()
+        while (rs.next()) {
+            val row = new JSONObject()
+            for (i <- 1 to metadata.getColumnCount) {
+                val key = metadata.getColumnLabel(i)
+                val value = rs.getObject(i)
+                row.put(key, value)
+            }
+            result += row
+        }
+        statement.close()
+        result
+    }
 
 }
