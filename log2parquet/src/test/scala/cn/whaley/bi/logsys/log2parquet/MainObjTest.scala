cf96077f2c2946a4724f1ff00410f06ec6261c57
wang.baozhi <286790238@qq.com>
2017-07-01 20:23:25 +0800

    "logType"特殊处理：helios-whaleyvip-activity

--- log2parquet/src/test/scala/cn/whaley/bi/logsys/log2parquet/MainObjTest.scala
+++ log2parquet/src/test/scala/cn/whaley/bi/logsys/log2parquet/MainObjTest.scala
@@ -1,6 +1,8 @@
 package cn.whaley.bi.logsys.log2parquet
 
 import cn.whaley.bi.logsys.log2parquet.traits.LogTrait
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.SparkSession
 import org.junit.Test
 
 /**
@@ -14,19 +16,18 @@ class MainObjTest extends LogTrait{
     @Test
     def testMsgProce: Unit = {
         val args = Array("MsgProcExecutor","--c","inputPath=/data_warehouse/ods_origin.db/log_origin/key_appId=boikgpokn78sb95ktmsc1bnkechpgj9l/key_day=20170614/key_hour=13/boikgpokn78sb95ktmsc1bnkechpgj9l_2017061413_raw_7_575892351.json.gz")
-       //val args = Array("MsgProcExecutor","--c","prop.inputPath=/Users/baozhiwang/Documents/upload_dir/a.json.gz")
-        MainObj.main(args)
+         MainObj.main(args)
 
     }
 
-
-   /* @Test
-    def testMsgProcExecutor: Unit = {
-        val args = Array("")
-        val executor = new MsgProcExecutor()
-        executor.execute(args)
-        executor.shutdown(true)
-        LOG.info("test completed.")
-    }*/
+    @Test
+    def testSparkSession(): Unit ={
+        val inputPath="/data_warehouse/ods_origin.db/log_origin/key_appId=boikgpokn78sb95ktmsc1bnkechpgj9l/key_day=20170614/key_hour=13/boikgpokn78sb95ktmsc1bnkechpgj9l_2017061413_raw_7_575892351.json.gz"
+        val config = new SparkConf()
+        config.setMaster("local[2]")
+        val sparkSession: SparkSession = SparkSession.builder().config(config).getOrCreate()
+        val rdd=sparkSession.sparkContext.textFile(inputPath, 2)
+        println(rdd.count())
+    }
 
 }
