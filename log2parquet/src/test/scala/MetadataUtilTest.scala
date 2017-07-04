6d91194ca9787400c3525d01abe10ddced6ba967
wang.baozhi <286790238@qq.com>
2017-07-04 15:12:59 +0800

    modify test structure

--- log2parquet/src/test/scala/MetadataUtilTest.scala
+++ log2parquet/src/test/scala/MetadataUtilTest.scala
@@ -73,19 +73,4 @@ class MetadataUtilTest {
 
         println("whaleytv".r.findFirstMatchIn("ods_view.db/log_whaleytv_wui20/key_day=20170630/key_hour=04").isDefined)
     }
-
-    @Test
-    def testMainObjTest: Unit = {
-        val args = Array("MsgProcExecutor","--c","inputPath=/data_warehouse/ods_origin.db/log_origin/key_appId=boikgpokn78sb95ktmsc1bnkechpgj9l/key_day=20170614/key_hour=13/boikgpokn78sb95ktmsc1bnkechpgj9l_2017061413_raw_7_575892351.json.gz","--c","masterURL=local[2]")
-        MainObj.main(args)
-    }
-
-    @Test
-    def parseSQLFieldInfos(): Unit ={
-        val path=new Path("/data_warehouse/ods_origin.db/log_origin/key_appId=boikgpokn78sb95ktmsc1bnkechpgj9l/key_day=20170614/key_hour=13/boikgpokn78sb95ktmsc1bnkechpgj9l_2017061413_raw_7_575892351.json.gz")
-        val result=ParquetHiveUtils.parseSQLFieldInfos(path)
-        result.map(e=>println(e))
-    }
-
-
 }
