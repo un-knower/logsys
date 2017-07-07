bb62fe20953e8b79082ae0cb4105d0928deb4e78
fj <master_fengj@163.com>
2017-07-07 16:10:32 +0800

    模块代码提交

--- ods-view-metadata/src/test/java/cn/whaley/bi/logsys/metadata/HiveRepoTest.java
+++ ods-view-metadata/src/test/java/cn/whaley/bi/logsys/metadata/HiveRepoTest.java
@@ -1,13 +1,17 @@
 package cn.whaley.bi.logsys.metadata;
 
 
+import cn.whaley.bi.logsys.metadata.entity.HiveFieldInfo;
 import cn.whaley.bi.logsys.metadata.repository.HiveRepo;
+import cn.whaley.bi.logsys.metadata.service.HiveUtil;
 import org.junit.Before;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.List;
 
 
 /**
@@ -31,6 +35,24 @@ public class HiveRepoTest {
         String tabName = "t_metadata_log";
         Boolean exists = repo.tabExists(dbName, tabName);
         LOG.info(String.format("%s.%s exists:%s", dbName, tabName, exists));
+    }
+
+    @Test
+    public void Test2() {
+        String dbName = "ods_view";
+        String tabName = "log_medusa_main3x_switchonoff";
+        List<HiveFieldInfo> fieldInfos = repo.getTabFieldInfo(dbName, tabName);
+        for (HiveFieldInfo fieldInfo : fieldInfos) {
+            LOG.info("{} {} {}", fieldInfo.getPartitionField(), fieldInfo.getColName(), fieldInfo.getDataType());
+        }
+    }
+
+    @Test
+    public void Test3() {
+        String oldFieldType = "struct<logSignFlag:bigint,msgFormat:string,msgId:string,msgSignFlag:bigint,msgSite:string,msgSource:string,msgVersion:string>";
+        String newFieldType = "struct<logSignFlag:bigint,msgFormat:string,msgId:string,msgSignFlag:bigint,msgSite:string,msgSource:string,msgVersion:string>";
+        Boolean convertible = HiveUtil.implicitConvertible(oldFieldType, newFieldType);
+        LOG.info("{}:{}->{}", convertible, oldFieldType, newFieldType);
     }
 
 
