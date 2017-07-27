5df0154cd4a21c18bd0331799c0ddda7ee56810f
wang.baozhi <286790238@qq.com>
2017-07-27 16:28:40 +0800

    增加按天运行脚本和azakban配置

--- log2parquet/src/main/sbin/log2parquetDay.sh
+++ log2parquet/src/main/sbin/log2parquetDay.sh
@@ -5,15 +5,24 @@ pwd=`pwd`
 echo "${pwd}"
 service_bin_home="../sbin"
 
-usage() { echo "Usage: $0 [-p <string> -d <string>]" 1>&2; exit 1; }
+usage() { echo "Usage: $0 [-p <string:path> -d <string:day> -j <string:isJsonDirDelete> -t <string:isTmpDirDelete> -f <string:taskFlag>]" 1>&2; exit 1; }
 
-while getopts ":d:h:" o; do
+while getopts ":p:d:j:t:f:" o; do
     case "${o}" in
         p)
             p=${OPTARG}
             ;;
         d)
             d=${OPTARG}
+            ;;
+        j)
+            j=${OPTARG}
+            ;;
+        t)
+            t=${OPTARG}
+            ;;
+        f)
+            f=${OPTARG}
             ;;
         *)
             usage
@@ -22,17 +31,14 @@ while getopts ":d:h:" o; do
 done
 shift $((OPTIND-1))
 
-if [ -z "${d}" ]; then
+if [ -z "${p}" ] || [ -z "${d}" ] || [ -z "${j}" ] || [ -z "${t}" ] || [ -z "${f}" ]; then
     usage
 fi
 
-echo "day = ${d}"
 
 #日期减去一天，获得新的day
 newDate=`date -d "${d} -1 day" +"%Y%m%d"`
 echo "newDate:${newDate}"
-
-echo "path:${path},newDate:${newDate}"
-inputPath=${path}/key_day=${newDate}
-echo "${inputPath}"
-sh ${service_bin_home}/submit.sh cn.whaley.bi.logsys.log2parquet.MainObj MsgProcExecutor --f MsgBatchManagerV3.xml,settings.properties --c inputPath=${inputPath}
+inputPath=${p}/key_day=${newDate}
+echo "inputPath:${inputPath},newDate:${newDate},isJsonDirDelete:${j},isTmpDirDelete:${t},taskFlag:${f}"
+sh ${service_bin_home}/submit.sh cn.whaley.bi.logsys.log2parquet.MainObj MsgProcExecutor --f MsgBatchManagerV3.xml,settings.properties --c inputPath=${inputPath} --c isJsonDirDelete=${j} --c isTmpDirDelete=${t} --c taskFlag=${f}
